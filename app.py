d6ea4bf6989caf49e9b0e346527efa71e11e8e11
Ali Zaib <alizaibkhanstatus@gmail.com>
2025-02-02 00:25:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -148,5 +148,5 @@ def add_no_cache_headers(response):
     return response
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=not is_koyeb)
