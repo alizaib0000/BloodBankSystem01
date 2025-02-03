a04ecdfd9d55e731fcf1f8d5eadc802ddcf497f3
Ali Zaib <alizaibkhanstatus@gmail.com>
2025-02-03 14:22:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -45,9 +45,11 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+    
 @app.route('/features')
 def features():
     return render_template('features.html')
+    
 @app.route('/contact')
 def contact):
     return render_template('contact.html')
