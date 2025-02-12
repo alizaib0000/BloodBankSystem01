b3523fa2d8c75799777168580f3bf503a6b04936
Ali Zaib <alizaibkhanstatus@gmail.com>
2025-02-12 18:36:07 +0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,28 +1,3 @@
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/67ac9fa73a842732607dd0f1/1ijt4v9lt';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-</script>
-<!--End of Tawk.to Script-->
-
-
-
-
-
-
-
-
-
-
-
-
 // Simulating Donor Count Update and Progress Bar Fill
 let donorCount = 0;  // Starting point
 const targetDonors = 5000;  // Total target donors
